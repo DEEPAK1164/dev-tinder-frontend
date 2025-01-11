109f6cfcf93963dd16c8e31dfeaea46f95f354c0
deepak <2001dkmaurya@gmail.com>
2025-01-11 22:47:09 +0530

    made-base-url-dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
-export const BASE_URL="/api";
+
+//production
+// export const BASE_URL="/api";
+
+//dev
+// export const BASE_URL="http://localhost:7777"
+
+
+export const BASE_URL=
+location.hostname==="localhost"?"http://localhost:7777":"/api";
